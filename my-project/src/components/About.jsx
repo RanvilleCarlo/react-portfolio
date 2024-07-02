242d09da360e00103224349f99345b1dd894bbcf
ranville <21c46.ranville@sjec.ac.in>
2024-07-02 16:54:09 +0530

    change

---
+++ my-project/src/components/About.jsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <div className="boa"></div>
+}
+
+export default About
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/RLogoF.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
     return (
-        <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
-            <div className="bg-red-mb-20 flex flex-shrink-0 items center">
-                <img src={logo} alt="logo"/>
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items center">
+                <img className="mx-2 w-12" src={logo} alt="logo"/>
 
             </div>
-            <div className="m-8 flex items-center justify-between gap-4 text-2xl">
+            <div className="m-6 flex items-center justify-between gap-6 text-3xl">
                 <FaLinkedin/>
                 <FaGithub/>
                 <FaSquareXTwitter/>
